ff6dccefbe112edf4bce22aed87286b7cc09f9b0
Mopro007 <clashmastercr7@gmail.com>
2023-09-22 15:16:39 +0300

    testing

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
     <div className="main_container">
-        <h1 className='title'>Team50</h1>
+        <h1 className='title'>SHIT50</h1>
             <Provider store={store}>
                 <App/>
             </Provider>
